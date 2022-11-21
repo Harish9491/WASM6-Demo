d1ddabf137d19dba5ebfbbc03f4bb63493ab56d3
haris <haris@HARISH-PC>
2022-11-21 11:34:35 +0530

    commit 1 - eclipse workspace user 2

--- src/test/java/demo/SampleTest.java
+++ src/test/java/demo/SampleTest.java
@@ -10,6 +10,8 @@ public class SampleTest {
 		System.out.println("Hi this is sample test");
 		
 		System.out.println("Hi this is maven projects workspace-User1");
+		
+		System.out.println("Hi this is user 2 - eclipse work space");
 	}
 
 }
