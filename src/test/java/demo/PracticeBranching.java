539a2b10b80ab00893cf143e708bace79389124e
haris <haris@HARISH-PC>
2022-11-22 11:17:54 +0530

    Branch 1: TC-Contacts_01

---
+++ src/test/java/demo/PracticeBranching.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class PracticeBranching {
+	
+	@Test
+	public void branch()
+	{
+		System.out.println("Practice branching and review");
+	}
+
+}
